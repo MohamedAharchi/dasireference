bfede7f770636c816b6f12f7e914ea178bae904b
Mohamed <aharchi.mohamed@gmail.com>
2016-01-07 08:54:24 +0100

    Mohamed - Update Menu and update account modal

--- reference/vues/v_account.php
+++ reference/vues/v_account.php
@@ -1,19 +1,19 @@
 <?php include 'v_menu.php'; ?>
         <div class="container" style="margin-top:100px;">
             <div class="col-md-12">
-                <div class="col-md-3" style="background-color: #2eb6ea;">
+                <div class="col-md-4"></div>
+                <div class="col-md-4" style="background-color: #2eb6ea;">
                     <img class="profile-img" src="images/icon-account.png" alt="Avatar" />
                     <ul style="list-style: none;">
-                        <li><strong>Lastname : </strong></li>
-                        <li><strong>Firstname : </strong></li>
-                        <li><strong>Email : </strong></li>
-                        <li><strong>Class : </strong></li>
-                        <li><strong>Session : </strong></li>
+                        <li><strong>Lastname : </strong>SIGNORET</li>
+                        <li><strong>Firstname : </strong>Alexandre</li>
+                        <li><strong>Email : </strong>alexandre.signoret@etu.univ-lyon1.fr</li>
+                        <li><strong>Class : </strong>DASI 2</li>
+                        <li><strong>Session : </strong>2015/2016<span style="margin-left: 50px;"><a href="#contact" class="btn btn-default" data-toggle="modal">Contact</a></span></li>
                     </ul>
                     <hr />
                 </div>
-                <div class="col-md-1"></div>
-                <div class="col-md-9"></div>
+                <div class="col-md-4"></div>
             </div>
         </div>
 <?php include'vues/v_contact.php';  ?>   
--- reference/vues/v_menu.php
+++ reference/vues/v_menu.php
@@ -14,25 +14,14 @@
               <!-- Collect the nav links, forms, and other content for toggling -->
               <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                  <li><a href="index.php?uc=schedule">Schedule</a></li>
-                  <li><a href="index.php?uc=homework">Homework</a></li>
-                  <li><a href="index.php?uc=lessons">Lessons</a></li>
-                  <li><a href="index.php?uc=link">Useful links</a></li>
-                  <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="#">Action</a></li>
-                      <li><a href="#">Another action</a></li>
-                      <li><a href="#">Something else here</a></li>
-                      <li role="separator" class="divider"></li>
-                      <li><a href="#">Separated link</a></li>
-                      <li role="separator" class="divider"></li>
-                      <li><a href="#">One more separated link</a></li>
-                    </ul>
-                  </li>                  
-                  <li><a href="#contact" data-toggle="modal">Contact</a></li>
-                  <li><a href="index.php?uc=account">My Account</a></li>
-                  <li><a href="index.php?uc=connexion&action=disconnect" class="btn btn_default" onclick="return confirm('Do you really want to disconnect ?')">Disconnect</a></li>
+                    <li><a href="index.php?uc=home">Home</a></li>
+                    <li><a href="index.php?uc=schedule">Schedule</a></li>
+                    <li><a href="index.php?uc=homework">Homework</a></li>
+                    <li><a href="index.php?uc=lessons">Lessons</a></li>
+                    <li><a href="index.php?uc=link">Useful links</a></li>
+                    <li><a href="#contact" data-toggle="modal">Contact</a></li>
+                    <li><a href="index.php?uc=account">My Account</a></li>
+                    <li><a href="index.php?uc=connexion&action=disconnect" class="btn btn_default" onclick="return confirm('Do you really want to disconnect ?')">Disconnect</a></li>
                 </ul>
               </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid -->
