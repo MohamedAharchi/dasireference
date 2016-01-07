fcada601296615cc0d0a724c246120c84280c2d5
cbeck5 <caroline.beckrich5@gmail.com>
2016-01-07 09:42:45 +0100

    Modifications add, homeworks

--- reference/vues/v_homework.php
+++ reference/vues/v_homework.php
@@ -1,16 +1,75 @@
 <?php include 'v_menu.php'; ?>
-        <div class="container" style="margin-top:100px;">
-            <div class="col-md-12">
-                <div class="jumbotron col-md-10">
-                                   
-                </div>
-            </div>
-      
-        </div>
-<?php include'vues/v_contact.php';  ?>   
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('.classysocial').ClassySocial();
-    });
-</script>   
+<div class="container" style="margin-top:100px;">
+    <div class="col-md-12">
+        <div class="jumbotron col-md-10">
+            <center>
+                <h1>Homeworks</h1></center>
+            
+                <b>Week 2</b>
+                <ul>
+                <li>9/11/2015 </li> 
+                <ul>
+                    <li>Java - Finir projet</li>
+                </ul>
+                    <li>10/11/2015 </li>
+                    <ul>
+                    <li>UML - DS sur le chapitre 2 </li>
+                    <li>UNIX - DS </li>
+</ul>
+                    <li>13/11/2015 </li>
+                    <ul>
+                    <li>BDD - Lire le cours "Révisions PL SQL"</li>
+                    
+                </ul></ul>
+            </ul>
+</br>
+
+            <b>Week 3</b>
+            <ul>
+                <li>22/11/2015</li>
+                <ul>
+                    <li>TP java - Finish application</li>
+                    <li>UML - Quizz chap 3</li>
+                    <li>Admin Unix - Quizz</li>
+                </ul>
+            </ul>
+            
+            </br>
+            
+            <b>Week 4</b>
+            <ul>
+                <li>07/12/2015</li>
+                <ul>
+                    <li>Java - Finish TP</li>
+                    <li>UML - Quizz</li>
+                    
+                </ul>
+            </ul>
+            
+            </br>
+            <b>Week 5</b>
+            <ul>
+                <li>04/01/2016</li>
+                <ul>
+                    <li>Java - Finish 2 TP java (héritage + cryptage)</li>
+                    <li>COM - Diaporama activity society COM</li>
+                </ul>
+                    <li>07/01/2016</li>
+                <ul>
+                        <li>English - Last seance project</li>
+                        <li>BDD - Quizz</li>
+                        <li>UNIX - Finish Script</li>
+                    </ul></ul>
+                </ul>
+
+                    </div>
+                    </div>
+
+                    </div>
+                    <?php include'vues/v_contact.php'; ?>   
+                    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+                    <script>
+                        $(document).ready(function () {
+                            $('.classysocial').ClassySocial();
+                        });
+                    </script>   
